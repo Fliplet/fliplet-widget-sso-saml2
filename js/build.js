052244024a83dee2a2a11d4e88354257424b0b6d
Ricardo Seromenho <ricardo@seromenho.com>
2017-07-19 10:14:00 +0100

    Redirect to authorize and resolve on auth complete

--- js/build.js
+++ js/build.js
@@ -1,8 +1,20 @@
 Fliplet.Widget.register('com.fliplet.sso.saml2', function registerComponent() {
   return {
     authorize: function (opts) {
+      return new Promise(function(resolve, reject) {
+        Fliplet.Navigate.to({
+          action: 'url',
+          inAppBrowser: true,
+          url: Fliplet.Env.get('apiUrl') + 'v1/session/authorize/saml2?' + Fliplet.Env.get('appId'),
+          onmessage: function(message) {
+            // resolve when authentication flow is done
+            resolve(message);
+          }
+        })
+      });
+      
       // redirect to v1/session/authorize/saml2?appId=1
-      return Promise.resolve(); // resolve when authentication flow is done
+      return Promise.resolve(); 
     }
   }
 });
