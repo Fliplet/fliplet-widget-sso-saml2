9e0350662749b6ab222aa3d8bba0db73615d4228
Nicholas Valbusa <squallstar@gmail.com>
2017-08-23 12:41:49 +0200

    sync action

--- js/build.js
+++ js/build.js
@@ -2,25 +2,23 @@ Fliplet.Widget.register('com.fliplet.sso.saml2', function registerComponent() {
   return {
     authorize: function (opts) {
       return new Promise(function(resolve, reject) {
-        Fliplet.Session.get().then(function () {
-          Fliplet.Navigate.to({
-            action: 'url',
-            inAppBrowser: true,
-            url: Fliplet.Env.get('apiUrl') + 'v1/session/authorize/saml2?appId=' + Fliplet.Env.get('appId') + '&auth_token=' + Fliplet.User.getAuthToken(),
-            // onmessage: function(message) {
-            //   // resolve when authentication flow is done
-            //   resolve(message);
-            // },
-            onclose: function () {
-              Fliplet.Session.get().then(function (session) {
-                if (session.server.passports.saml2) {
-                  return resolve();
-                }
+        Fliplet.Navigate.to({
+          action: 'url',
+          inAppBrowser: true,
+          url: Fliplet.Env.get('apiUrl') + 'v1/session/authorize/saml2?appId=' + Fliplet.Env.get('appId') + '&auth_token=' + Fliplet.User.getAuthToken(),
+          // onmessage: function(message) {
+          //   // resolve when authentication flow is done
+          //   resolve(message);
+          // },
+          onclose: function () {
+            Fliplet.Session.get().then(function (session) {
+              if (session.server.passports.saml2) {
+                return resolve();
+              }
 
-                reject('Not logged in');
-              });
-            }
-          });
+              reject('Not logged in');
+            });
+          }
         });
       });
     }
