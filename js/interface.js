2bf1865ccaadb4a4760dcb23abadae5133854a48
Ricardo Seromenho <ricardo@seromenho.com>
2017-09-05 14:54:52 +0100

    fix autosize on load

--- js/interface.js
+++ js/interface.js
@@ -1,8 +1,6 @@
 Fliplet().then(function () {
   var appId = Fliplet.Env.get('appId');
 
-  $(window).on('resize', Fliplet.Widget.autosize);
-
   $('#entity_id').html(
     appId
     ? Fliplet.Env.get('apiUrl') + 'v1/session/providers/saml2/metadata?appId=' + appId + '&auth_token=' + Fliplet.User.getAuthToken()
@@ -28,4 +26,6 @@ Fliplet().then(function () {
   Fliplet.Widget.onSaveRequest(function () {
     $('form').submit();
   });
+
+  Fliplet.Widget.autosize();
 });
