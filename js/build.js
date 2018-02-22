9fc75dd52adf6e2564048cfaadc2527bb4f2da27
Nicholas Valbusa <squallstar@gmail.com>
2018-02-22 16:06:58 +0100

    support for basic auth
    basic auth

--- js/build.js
+++ js/build.js
@@ -1,12 +1,24 @@
 Fliplet.Widget.register('com.fliplet.sso.saml2', function registerComponent() {
   return {
     authorize: function(opts) {
+      opts = opts || {};
+
       return new Promise(function(resolve, reject) {
+        var url = Fliplet.Env.get('apiUrl') + 'v1/session/authorize/saml2?appId=' + Fliplet.Env.get('appId') + '&auth_token=' + Fliplet.User.getAuthToken();
+
+        if (opts.username) {
+          url += '&username' + encodeURIComponent(opts.username);
+        }
+
+        if (opts.password) {
+          url += '&password' + encodeURIComponent(opts.password);
+        }
+
         Fliplet.Navigate.to({
           action: 'url',
           inAppBrowser: true,
           handleAuthorization: false,
-          url: Fliplet.Env.get('apiUrl') + 'v1/session/authorize/saml2?appId=' + Fliplet.Env.get('appId') + '&auth_token=' + Fliplet.User.getAuthToken(),
+          url: url,
           onclose: function() {
             Fliplet.Session.get().then(function(session) {
               if (session.server.passports.saml2) {
