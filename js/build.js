decd623f083b94432b22812a1fa721aa87e1e6da
Nicholas Valbusa <squallstar@gmail.com>
2018-03-02 13:39:45 +0100

    don't check for xhr

--- js/build.js
+++ js/build.js
@@ -8,6 +8,7 @@ Fliplet.Widget.register('com.fliplet.sso.saml2', function registerComponent() {
           action: 'url',
           inAppBrowser: true,
           basicAuth: opts.basicAuth,
+          handleAuthorization: false,
           url: Fliplet.Env.get('apiUrl') + 'v1/session/authorize/saml2?appId=' + Fliplet.Env.get('appId') + '&auth_token=' + Fliplet.User.getAuthToken(),
           onclose: function() {
             Fliplet.Session.get().then(function(session) {
