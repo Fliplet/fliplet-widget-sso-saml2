4682f4e6e6cc29d0d6056fad296819a2e34ce55e
Nicholas Valbusa <squallstar@gmail.com>
2020-03-24 18:42:11 +0100

    use safari on ios12 for logging in

--- js/build.js
+++ js/build.js
@@ -3,10 +3,18 @@ Fliplet.Widget.register('com.fliplet.sso.saml2', function registerComponent() {
     authorize: function(opts) {
       opts = opts || {};
 
+      var inAppBrowser = true;
+
+      // Use Safari on iOS12 to prevent issues with cookies not being saved.
+      // ref: https://www.chromium.org/updates/same-site/incompatible-clients
+      if (Modernizr.ios && Fliplet.Navigator.device().version.indexOf('12') === 0) {
+        inAppBrowser = false;
+      }
+
       return new Promise(function(resolve, reject) {
         Fliplet.Navigate.to({
           action: 'url',
-          inAppBrowser: true,
+          inAppBrowser: inAppBrowser,
           basicAuth: opts.basicAuth,
           handleAuthorization: false,
           url: (Fliplet.Env.get('primaryApiUrl') || Fliplet.Env.get('apiUrl')) + 'v1/session/authorize/saml2?appId=' + Fliplet.Env.get('masterAppId') + '&auth_token=' + Fliplet.User.getAuthToken(),
